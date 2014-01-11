d667c03fab1a3d78da67657e4398def05a604c0e
vagrant <vagrant@dev-in-rails-box>
2014-01-11 15:49:10 +0000

    Added install script

---
+++ .vim/bundle/install.sh
@@ -0,0 +1,16 @@
+git clone https://github.com/kien/ctrlp.vim.git;
+git clone https://github.com/scrooloose/nerdtree.git;
+git clone https://github.com/msanders/snipmate.vim.git;
+git clone https://github.com/scrooloose/syntastic.git;
+git clone https://github.com/tpope/vim-bundler.git;
+git clone https://github.com/kchmck/vim-coffee-script.git;
+git clone https://github.com/tpope/vim-commentary.git;
+git clone https://github.com/Lokaltog/vim-easymotion.git;
+git clone https://github.com/tpope/vim-eunuch.git;
+git clone https://github.com/tpope/vim-fugitive.git;
+git clone https://github.com/tpope/vim-haml.git;
+git clone https://github.com/tpope/vim-rails.git;
+git clone https://github.com/vim-ruby/vim-ruby.git;
+git clone https://github.com/tpope/vim-sensible.git;
+git clone https://github.com/tpope/vim-surround.git;
+
